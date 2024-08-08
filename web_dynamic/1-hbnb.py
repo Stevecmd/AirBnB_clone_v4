6c92a2df70ce2d24822680c0c7a4e716b6939eae
SteveCmd <murimi101@gmail.com>
2024-08-08 16:08:40 +0300

    chore: Update Flask app to use new template folder for web_dynamic - 1. Cash only

---
+++ web_dynamic/1-hbnb.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template, redirect
+import logging
+import uuid
+from jinja2 import TemplateNotFound
+app = Flask(__name__, template_folder='web_flask/templates/')
+app.logger.setLevel(logging.DEBUG)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('1-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid.uuid4()
+                           )
+
+
+@app.route('/1-hbnb', strict_slashes=False)
+def one_hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    try:
+        return render_template('1-hbnb.html',
+                               states=st_ct,
+                               amenities=amenities,
+                               places=places,
+                               cache_id=uuid.uuid4()
+                               )
+    except TemplateNotFound:
+        app.logger.error("Template '1-hbnb.html' not found.")
+        return "Template not found", 404
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000, debug=True)
