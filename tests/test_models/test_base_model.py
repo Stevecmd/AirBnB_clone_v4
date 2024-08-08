c24820c824b87a92db8c0df29be99d7a35d303b0
Restyled.io <commits@restyled.io>
2024-08-08 06:14:46 +0000

    Restyled by black

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ import pep8 as pycodestyle
 import time
 import unittest
 from unittest import mock
+
 BaseModel = models.base_model.BaseModel
 module_doc = models.base_model.__doc__
 
@@ -21,25 +22,22 @@ class TestBaseModelDocs(unittest.TestCase):
 
     def test_pep8_conformance(self):
         """Test that models/base_model.py conforms to PEP8."""
-        for path in ['models/base_model.py',
-                     'tests/test_models/test_base_model.py']:
+        for path in ["models/base_model.py", "tests/test_models/test_base_model.py"]:
             with self.subTest(path=path):
                 errors = pycodestyle.Checker(path).check_all()
                 self.assertEqual(errors, 0)
 
     def test_module_docstring(self):
         """Test for the existence of module docstring"""
-        self.assertIsNot(module_doc, None,
-                         "base_model.py needs a docstring")
-        self.assertTrue(len(module_doc) > 1,
-                        "base_model.py needs a docstring")
+        self.assertIsNot(module_doc, None, "base_model.py needs a docstring")
+        self.assertTrue(len(module_doc) > 1, "base_model.py needs a docstring")
 
     def test_class_docstring(self):
         """Test for the BaseModel class docstring"""
-        self.assertIsNot(BaseModel.__doc__, None,
-                         "BaseModel class needs a docstring")
-        self.assertTrue(len(BaseModel.__doc__) >= 1,
-                        "BaseModel class needs a docstring")
+        self.assertIsNot(BaseModel.__doc__, None, "BaseModel class needs a docstring")
+        self.assertTrue(
+            len(BaseModel.__doc__) >= 1, "BaseModel class needs a docstring"
+        )
 
     def test_func_docstrings(self):
         """Test for the presence of docstrings in BaseModel methods"""
@@ -48,11 +46,11 @@ class TestBaseModelDocs(unittest.TestCase):
                 self.assertIsNot(
                     func[1].__doc__,
                     None,
-                    "{:s} method needs a docstring".format(func[0])
+                    "{:s} method needs a docstring".format(func[0]),
                 )
                 self.assertTrue(
                     len(func[1].__doc__) > 1,
-                    "{:s} method needs a docstring".format(func[0])
+                    "{:s} method needs a docstring".format(func[0]),
                 )
 
 
@@ -70,7 +68,7 @@ class TestBaseModel(unittest.TestCase):
             "created_at": datetime,
             "updated_at": datetime,
             "name": str,
-            "number": int
+            "number": int,
         }
         for attr, typ in attrs_types.items():
             with self.subTest(attr=attr, typ=typ):
@@ -89,14 +87,16 @@ class TestBaseModel(unittest.TestCase):
         toc = datetime.utcnow()
         self.assertTrue(
             tic <= inst1.created_at <= toc,
-            f"inst1.created_at {inst1.created_at} not in range {tic} - {toc}")
+            f"inst1.created_at {inst1.created_at} not in range {tic} - {toc}",
+        )
         time.sleep(1e-4)
         tic = datetime.utcnow()
         inst2 = BaseModel()
         toc = datetime.utcnow()
-        self.assertTrue(tic <= inst2.created_at <= toc,
-                        f"inst2.created_at {inst2.created_at} "
-                        f"not in range {tic} - {toc}")
+        self.assertTrue(
+            tic <= inst2.created_at <= toc,
+            f"inst2.created_at {inst2.created_at} " f"not in range {tic} - {toc}",
+        )
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
@@ -110,10 +110,12 @@ class TestBaseModel(unittest.TestCase):
             uuid = inst.id
             with self.subTest(uuid=uuid):
                 self.assertIs(type(uuid), str)
-                self.assertRegex(uuid,
-                                 '^[0-9a-f]{8}-[0-9a-f]{4}'
-                                 '-[0-9a-f]{4}-[0-9a-f]{4}'
-                                 '-[0-9a-f]{12}$')
+                self.assertRegex(
+                    uuid,
+                    "^[0-9a-f]{8}-[0-9a-f]{4}"
+                    "-[0-9a-f]{4}-[0-9a-f]{4}"
+                    "-[0-9a-f]{12}$",
+                )
         self.assertNotEqual(inst1.id, inst2.id)
 
     def test_to_dict(self):
@@ -122,16 +124,18 @@ class TestBaseModel(unittest.TestCase):
         my_model.name = "Holberton"
         my_model.my_number = 89
         d = my_model.to_dict()
-        expected_attrs = ["id",
-                          "created_at",
-                          "updated_at",
-                          "name",
-                          "my_number",
-                          "__class__"]
+        expected_attrs = [
+            "id",
+            "created_at",
+            "updated_at",
+            "name",
+            "my_number",
+            "__class__",
+        ]
         self.assertCountEqual(d.keys(), expected_attrs)
-        self.assertEqual(d['__class__'], 'BaseModel')
-        self.assertEqual(d['name'], "Holberton")
-        self.assertEqual(d['my_number'], 89)
+        self.assertEqual(d["__class__"], "BaseModel")
+        self.assertEqual(d["name"], "Holberton")
+        self.assertEqual(d["my_number"], 89)
 
     def test_to_dict_values(self):
         """test that values in dict returned from to_dict are correct"""
@@ -150,7 +154,7 @@ class TestBaseModel(unittest.TestCase):
         string = "[BaseModel] ({}) {}".format(inst.id, inst.__dict__)
         self.assertEqual(string, str(inst))
 
-    @mock.patch('models.storage')
+    @mock.patch("models.storage")
     def test_save(self, mock_storage):
         """Test that save method updates `updated_at` and calls
         `storage.save`"""
