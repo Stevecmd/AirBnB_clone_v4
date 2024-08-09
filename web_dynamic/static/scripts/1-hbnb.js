b219d3d64a90b1a172413a464f32361523583dd6
Restyled.io <commits@restyled.io>
2024-08-09 19:28:16 +0000

    Restyled by prettier

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,19 @@
 /* global $ */
 
-$(document).ready(function() {
+$(document).ready(function () {
   const selectedAmenities = {};
 
-  $('input[type="checkbox"]').change(function() {
-    const amenityId = $(this).attr('data-id');
-    const amenityName = $(this).attr('data-name');
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr("data-id");
+    const amenityName = $(this).attr("data-name");
 
-    if ($(this).is(':checked')) {
+    if ($(this).is(":checked")) {
       selectedAmenities[amenityId] = amenityName;
     } else {
       delete selectedAmenities[amenityId];
     }
 
-    const amenitiesList = Object.values(selectedAmenities).join(', ');
-    $('.amenities h4').text(amenitiesList);
+    const amenitiesList = Object.values(selectedAmenities).join(", ");
+    $(".amenities h4").text(amenitiesList);
   });
 });
--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,20 +1,20 @@
 /* global $ */
 
-$(document).ready(function() {
+$(document).ready(function () {
   const selectedStates = {};
   const selectedCities = {};
 
-  $('input[type="checkbox"]').change(function() {
-    const id = $(this).data('id');
-    const name = $(this).data('name');
-    if ($(this).is(':checked')) {
-      if ($(this).closest('li').parent().parent().is('li')) {
+  $('input[type="checkbox"]').change(function () {
+    const id = $(this).data("id");
+    const name = $(this).data("name");
+    if ($(this).is(":checked")) {
+      if ($(this).closest("li").parent().parent().is("li")) {
         selectedCities[id] = name;
       } else {
         selectedStates[id] = name;
       }
     } else {
-      if ($(this).closest('li').parent().parent().is('li')) {
+      if ($(this).closest("li").parent().parent().is("li")) {
         delete selectedCities[id];
       } else {
         delete selectedStates[id];
@@ -24,71 +24,82 @@ $(document).ready(function() {
   });
 
   function updateLocations() {
-    const locations =
-        Object.values(selectedStates).concat(Object.values(selectedCities));
-    $('div.locations h4').text(locations.join(', '));
+    const locations = Object.values(selectedStates).concat(
+      Object.values(selectedCities),
+    );
+    $("div.locations h4").text(locations.join(", "));
   }
 
-  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+    if (data.status === "OK") {
+      $("#api_status").addClass("available");
     } else {
-      $('#api_status').removeClass('available');
+      $("#api_status").removeClass("available");
     }
   });
 
   function fetchPlaces(data) {
     $.ajax({
-      url : 'http://0.0.0.0:5001/api/v1/places_search/',
-      type : 'POST',
-      contentType : 'application/json',
-      data : JSON.stringify(data),
-      success : function(data) {
-        $('section.places').empty();
+      url: "http://0.0.0.0:5001/api/v1/places_search/",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(data),
+      success: function (data) {
+        $("section.places").empty();
         for (const place of data) {
-          const article = $('<article></article>');
+          const article = $("<article></article>");
           const titleBox = $('<div class="title_box"></div>');
           titleBox.append(`<h2>${place.name}</h2>`);
           titleBox.append(
-              `<div class="price_by_night">$${place.price_by_night}</div>`);
+            `<div class="price_by_night">$${place.price_by_night}</div>`,
+          );
           article.append(titleBox);
 
           const information = $('<div class="information"></div>');
-          information.append(`<div class="max_guest">${place.max_guest} Guest${
-              place.max_guest !== 1 ? 's' : ''}</div>`);
           information.append(
-              `<div class="number_rooms">${place.number_rooms} Bedroom${
-                  place.number_rooms !== 1 ? 's' : ''}</div>`);
-          information.append(`<div class="number_bathrooms">${
-              place.number_bathrooms} Bathroom${
-              place.number_bathrooms !== 1 ? 's' : ''}</div>`);
+            `<div class="max_guest">${place.max_guest} Guest${
+              place.max_guest !== 1 ? "s" : ""
+            }</div>`,
+          );
+          information.append(
+            `<div class="number_rooms">${place.number_rooms} Bedroom${
+              place.number_rooms !== 1 ? "s" : ""
+            }</div>`,
+          );
+          information.append(
+            `<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+              place.number_bathrooms !== 1 ? "s" : ""
+            }</div>`,
+          );
           article.append(information);
 
           const user = $('<div class="user"></div>');
           user.append(
-              `<b>Owner:</b> ${place.user.first_name} ${place.user.last_name}`);
+            `<b>Owner:</b> ${place.user.first_name} ${place.user.last_name}`,
+          );
           article.append(user);
 
           const description = $('<div class="description"></div>');
           description.html(place.description);
           article.append(description);
 
-          $('section.places').append(article);
+          $("section.places").append(article);
         }
-      }
+      },
     });
   }
 
   fetchPlaces({});
 
-  $('button').click(function() {
+  $("button").click(function () {
     const amenities = [];
-    $('input[type="checkbox"]:checked')
-        .each(function() { amenities.push($(this).data('id')); });
+    $('input[type="checkbox"]:checked').each(function () {
+      amenities.push($(this).data("id"));
+    });
     fetchPlaces({
       amenities,
-      states : Object.keys(selectedStates),
-      cities : Object.keys(selectedCities)
+      states: Object.keys(selectedStates),
+      cities: Object.keys(selectedCities),
     });
   });
 });
